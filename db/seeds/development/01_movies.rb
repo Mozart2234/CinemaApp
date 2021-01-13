994611bf922b0533eea74bf421d3fc1b1e681478
Mozart2234 <alexeim763@gmail.com>
2021-01-12 22:42:34 -0500

    [WIP] - Added some movie seed

---
+++ db/seeds/development/01_movies.rb
@@ -0,0 +1,52 @@
+movies = [
+  {
+    budget: 200_000_000,
+    director: 'Dzhanik Fayziev',
+    imbd_ranking: 7.2,
+    plot: 'Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s and finds a formidable foe by the name of the Cheetah.',
+    poster: ,
+    revenue: 131400000,
+    running_time: 151,
+    title: 'Wonder Woman 1984'
+  },
+  {
+    budget: 200_000_000,
+    director: 'Dzhanik Fayziev',
+    imbd_ranking: 7.2,
+    plot: 'Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s and finds a formidable foe by the name of the Cheetah.',
+    poster: ,
+    revenue: 131400000,
+    running_time: 151,
+    title: 'Wonder Woman 1984'
+  },
+  {
+    budget: 200_000_000,
+    director: 'Dzhanik Fayziev',
+    imbd_ranking: 7.2,
+    plot: 'Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s and finds a formidable foe by the name of the Cheetah.',
+    poster: ,
+    revenue: 131400000,
+    running_time: 151,
+    title: 'Wonder Woman 1984'
+  },
+  {
+    budget: 200_000_000,
+    director: 'Dzhanik Fayziev',
+    imbd_ranking: 7.2,
+    plot: 'Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s and finds a formidable foe by the name of the Cheetah.',
+    poster: ,
+    revenue: 131400000,
+    running_time: 151,
+    title: 'Wonder Woman 1984'
+  },
+  {
+    budget: 200_000_000,
+    director: 'Dzhanik Fayziev',
+    imbd_ranking: 7.2,
+    plot: 'Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s and finds a formidable foe by the name of the Cheetah.',
+    poster: ,
+    revenue: 131400000,
+    running_time: 151,
+    title: 'Wonder Woman 1984'
+  },
+]
