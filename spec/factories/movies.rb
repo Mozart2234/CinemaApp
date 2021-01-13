282a92a9ff24a079baa79e2011655b5f26498884
Mozart2234 <alexeim763@gmail.com>
2021-01-12 22:26:15 -0500

    Updated range for imbd_ranking in his factory

--- spec/factories/movies.rb
+++ spec/factories/movies.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :movie do
     budget { Faker::Commerce.price }
     director { Faker::Name.name  }
-    imbd_ranking { Faker::Number.number(from: 0.0, to: 5.0) }
+    imbd_ranking { Faker::Number.number(from: 0.0, to: 10.0) }
     plot { Faker::Movie.quote }
     poster { }
     ranking { Faker::Number.between(from: 0.0, to: 5.0).round(1) }
