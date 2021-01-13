0e44893a3b28f550c597db6a633a16f900f1c89d
Mozart2234 <alexeim763@gmail.com>
2021-01-12 23:44:34 -0500

    Finished dummy data for movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,6 @@ Dir[Rails.root.join('db', 'seeds', '*.rb')].sort.each do |seed|
   load seed
 end
 
-Dir[Rails.root.join('db', 'seeds', Rails.env, '*.env')].sort.each do |seed|
+Dir[Rails.root.join('db', 'seeds', Rails.env, '*.rb')].sort.each do |seed|
   load seed
 end
--- db/seeds/development/01_movies.rb
+++ db/seeds/development/01_movies.rb
@@ -1,3 +1,5 @@
+# poster_name is a column as not part of movie model
+# it is only used for to upload an image with active_storage
 movies = [
   {
     budget: 200_000_000,
@@ -6,7 +8,8 @@ movies = [
     plot: 'Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s and finds a formidable foe by the name of the Cheetah.',
     revenue: 131400000,
     running_time: 151,
-    title: 'Wonder Woman 1984'
+    title: 'Wonder Woman 1984',
+    poster_name: 'wonder_woman.jpg'
   },
   {
     budget: 150_000_000,
@@ -15,7 +18,8 @@ movies = [
     plot: 'Joe Gardner is a middle school teacher with a love for jazz music. After a successful gig at the Half Note Club, he suddenly gets into an accident that separates his soul from his body and is transported to the You Seminar, a center in which souls develop and gain passions before being transported to a newborn child. Joe must enlist help from the other souls-in-training, like 22, a soul who has spent eons in the You Seminar, in order to get back to Earth.',
     revenue: 123400000,
     running_time: 102,
-    title: 'Soul'
+    title: 'Soul',
+    poster_name: 'soul.jpg'
   },
   {
     budget: 150_000_000,
@@ -24,7 +28,8 @@ movies = [
     plot: 'Cosmoball is a mesmerizing intergalactic game of future played between humans and aliens at the giant extraterrestrial ship hovering in the sky over Earth. A young man with enormous power of an unknown nature joins the team of hot-headed superheroes in exchange for a cure for his mother’s deadly illness. The Four from Earth will fight not only to defend the honor of their home planet in the spectacular game, but to face the unprecedented threat to the Galaxy and embrace their own destiny.',
     revenue: 0,
     running_time: 115,
-    title: 'Cosmoball'
+    title: 'Cosmoball',
+    poster_name: 'cosmoball.jpg'
   },
   {
     budget: 150_000_000,
@@ -33,7 +38,8 @@ movies = [
     plot: 'Cosmoball is a mesmerizing intergalactic game of future played between humans and aliens at the giant extraterrestrial ship hovering in the sky over Earth. A young man with enormous power of an unknown nature joins the team of hot-headed superheroes in exchange for a cure for his mother’s deadly illness. The Four from Earth will fight not only to defend the honor of their home planet in the spectacular game, but to face the unprecedented threat to the Galaxy and embrace their own destiny.',
     revenue: 27900000,
     running_time: 107,
-    title: 'Vanguard'
+    title: 'Vanguard',
+    poster_name: 'vanguard.jpg'
   },
   {
     budget: 0.0,
@@ -42,6 +48,19 @@ movies = [
     plot: 'During a school field trip, Ladybug and Cat Noir meet the American superheroes, whom they have to save from an akumatised super-villain. They discover that Miraculous exist in the United States too.',
     revenue: 0,
     running_time: 0,
-    title: 'Miraculous World: New York, United HeroeZ'
+    title: 'Miraculous World: New York, United HeroeZ',
+    poster_name: 'miraculous.jpg'
   }
 ]
+
+puts "01 - Creating movies"
+movies.each do |movie_h|
+  poster_name = movie_h.delete(:poster_name)
+  movie = Movie.find_or_create_by(movie_h)
+  next unless movie.persisted?
+
+  path = Rails.root.join('db', 'seeds', 'static', 'movies', poster_name)
+  movie.poster.attace(io: File.open(path, file_name: poster_name))
+  puts "Movie: #{movie.title} was created"
+end
+puts "Movies Created"
