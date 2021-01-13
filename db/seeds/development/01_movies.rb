d12688819f8f9e4fd9def26651809416f5f0acd5
Mozart2234 <alexeim763@gmail.com>
2021-01-12 23:25:50 -0500

    Added some seed data

--- db/seeds/development/01_movies.rb
+++ db/seeds/development/01_movies.rb
@@ -4,49 +4,44 @@ movies = [
     director: 'Dzhanik Fayziev',
     imbd_ranking: 7.2,
     plot: 'Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s and finds a formidable foe by the name of the Cheetah.',
-    poster: ,
     revenue: 131400000,
     running_time: 151,
     title: 'Wonder Woman 1984'
   },
   {
-    budget: 200_000_000,
+    budget: 150_000_000,
     director: 'Dzhanik Fayziev',
-    imbd_ranking: 7.2,
-    plot: 'Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s and finds a formidable foe by the name of the Cheetah.',
-    poster: ,
-    revenue: 131400000,
-    running_time: 151,
-    title: 'Wonder Woman 1984'
+    imbd_ranking: 8.4,
+    plot: 'Joe Gardner is a middle school teacher with a love for jazz music. After a successful gig at the Half Note Club, he suddenly gets into an accident that separates his soul from his body and is transported to the You Seminar, a center in which souls develop and gain passions before being transported to a newborn child. Joe must enlist help from the other souls-in-training, like 22, a soul who has spent eons in the You Seminar, in order to get back to Earth.',
+    revenue: 123400000,
+    running_time: 102,
+    title: 'Soul'
   },
   {
-    budget: 200_000_000,
-    director: 'Dzhanik Fayziev',
-    imbd_ranking: 7.2,
-    plot: 'Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s and finds a formidable foe by the name of the Cheetah.',
-    poster: ,
-    revenue: 131400000,
-    running_time: 151,
-    title: 'Wonder Woman 1984'
+    budget: 150_000_000,
+    director: 'Fayziev',
+    imbd_ranking: 4.9,
+    plot: 'Cosmoball is a mesmerizing intergalactic game of future played between humans and aliens at the giant extraterrestrial ship hovering in the sky over Earth. A young man with enormous power of an unknown nature joins the team of hot-headed superheroes in exchange for a cure for his mother’s deadly illness. The Four from Earth will fight not only to defend the honor of their home planet in the spectacular game, but to face the unprecedented threat to the Galaxy and embrace their own destiny.',
+    revenue: 0,
+    running_time: 115,
+    title: 'Cosmoball'
   },
   {
-    budget: 200_000_000,
-    director: 'Dzhanik Fayziev',
-    imbd_ranking: 7.2,
-    plot: 'Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s and finds a formidable foe by the name of the Cheetah.',
-    poster: ,
-    revenue: 131400000,
-    running_time: 151,
-    title: 'Wonder Woman 1984'
+    budget: 150_000_000,
+    director: 'Fayziev',
+    imbd_ranking: 7.4,
+    plot: 'Cosmoball is a mesmerizing intergalactic game of future played between humans and aliens at the giant extraterrestrial ship hovering in the sky over Earth. A young man with enormous power of an unknown nature joins the team of hot-headed superheroes in exchange for a cure for his mother’s deadly illness. The Four from Earth will fight not only to defend the honor of their home planet in the spectacular game, but to face the unprecedented threat to the Galaxy and embrace their own destiny.',
+    revenue: 27900000,
+    running_time: 107,
+    title: 'Vanguard'
   },
   {
-    budget: 200_000_000,
-    director: 'Dzhanik Fayziev',
-    imbd_ranking: 7.2,
-    plot: 'Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s and finds a formidable foe by the name of the Cheetah.',
-    poster: ,
-    revenue: 131400000,
-    running_time: 151,
-    title: 'Wonder Woman 1984'
-  },
+    budget: 0.0,
+    director: 'Thomas Astruc',
+    imbd_ranking: 8.6,
+    plot: 'During a school field trip, Ladybug and Cat Noir meet the American superheroes, whom they have to save from an akumatised super-villain. They discover that Miraculous exist in the United States too.',
+    revenue: 0,
+    running_time: 0,
+    title: 'Miraculous World: New York, United HeroeZ'
+  }
 ]
