a2493693d78bb41345d23c23eb3f13a496845963
Mozart2234 <alexeim763@gmail.com>
2021-01-12 22:15:26 -0500

    Create a first version of movie factory

---
+++ spec/factories/movies.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :movie do
+    budget { Faker::Commerce.price }
+    director { Faker::Name.name  }
+    imbd_ranking { Faker::Number.number(from: 0.0, to: 5.0) }
+    plot { Faker::Movie.quote }
+    poster { }
+    ranking { Faker::Number.between(from: 0.0, to: 5.0).round(1) }
+    revenue { Faker::Commerce.price }
+    running_time { Faker::Number.between(from: 30.0, to: 120.0 ).round(2) }
+    title { Faker::Movie.title }
+  end
+end
